dfc79b48ee454895bb345eacdee2da9f7e96a400
Sheersh <sheershsinha30@gmail.com>
2025-12-16 15:32:45 +0530

    Added-Script-File

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+#Convert all letters in a string to uppercase using a `for` loop
+
+str="hello world"
+upper=""
+
+for (( i=0; i<${#str}; i++ )); do
+    ch="${str:$i:1}"
+    upper+=$(printf "%s" "$ch" | tr '[:lower:]' '[:upper:]')
+done
+
+echo "$upper"
